e6dfeb3bf9c887cd17b79486c1253a9e962356a1
Arphail <alexusoffd@gmail.com>
2022-10-14 22:52:54 +0300

    2nd Attempt

--- RandomSpawner.cs
+++ RandomSpawner.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 public class RandomSpawner : MonoBehaviour
 {
     [SerializeField] private SpawnPoint[] _spawnPoints;
-
+    
+    private bool _canSpawn;
     private float _respawnTime = 2f;
+    private WaitForSeconds _waitBeforeSpawn;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out Player player))
+            _canSpawn = false;
+    }
 
     private void Start()
     {
+        _canSpawn = true;
+        _waitBeforeSpawn = new WaitForSeconds(_respawnTime);
         StartCoroutine(WaitBeforeRespawn());
     }
 
     private IEnumerator WaitBeforeRespawn()
     {
-        while (FindObjectOfType<Player>())
+        while (_canSpawn)
         {
             foreach (var spawnPoint in _spawnPoints)
             {
                 if (spawnPoint.IsBusy == false)
-                {
-                    yield return new WaitForSeconds(_respawnTime);
                     spawnPoint.SpawnEnemy();
-                }
+
+                yield return _waitBeforeSpawn;
             }
         }
     }
