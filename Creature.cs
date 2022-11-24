fb39ef9f918902a47b84ff9fafa58372b16ac7d9
Arphail <alexusoffd@gmail.com>
2022-11-24 16:53:36 +0300

    1st Attempt

---
+++ Creature.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+    private float _maxHealth = 50;
+
+    public float CurrentHealth { get; private set; }
+
+    private void OnEnable()
+    {
+        CurrentHealth = _maxHealth;
+    }
+
+    private void Update()
+    {
+        if (CurrentHealth < 0)
+            CurrentHealth = 0;
+    }
+
+    public void TakeDamage(float damage) => CurrentHealth -= damage;
+    
+    public void Heal(float healingValue) => CurrentHealth += healingValue;
+}
---
+++ HealthBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Creature _creature;
+    [SerializeField] private float _healthChangeSpeed;
+
+    private Slider _healthBar;
+
+    private void Start()
+    {
+        _healthBar = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        Coroutine smoothHealthChanger = null;
+
+        if (_healthBar.value != _creature.CurrentHealth)
+            smoothHealthChanger = StartCoroutine(SmoothHealthChanger());
+
+        if (smoothHealthChanger == null)
+            StopCoroutine(smoothHealthChanger);
+    }
+
+    private IEnumerator SmoothHealthChanger()
+    {
+        while (_healthBar.value != _creature.CurrentHealth)
+        {
+            _healthBar.value = Mathf.MoveTowards(_healthBar.value, _creature.CurrentHealth, _healthChangeSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
